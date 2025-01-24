c6c95d0e752df1fdedd48a46dda97e86315b3336
Guilherme Saldanha <guiligeskee@gmail.com>
2025-01-24 15:41:46 -0300

    att 24/01/25

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,3 +1,7 @@
+const User = require("../models/User");
+
+const bcrypt = require("bcryptjs");
+
 module.exports = class AuthController {
   static login(req, res) {
     res.render("auth/login");
@@ -5,5 +9,45 @@ module.exports = class AuthController {
 
   static register(req, res) {
     res.render("auth/register");
+  }
+
+  static async registerPost(req, res) {
+    const { name, email, password, confirmpassword } = req.body;
+
+    // password match validation
+    if (password !== confirmpassword) {
+      req.flash("message", "As senhas não conferem, tente novamente!");
+      res.render("auth/register");
+
+      return;
+    }
+
+    // Check if user exists
+    const checkIfUserExists = await User.findOne({ where: { email: email } });
+
+    if (checkIfUserExists) {
+      req.flash("message", "O e-mail já está em uso, tente novamente!");
+      res.render("auth/register");
+
+      return;
+    }
+
+    // create a password
+    const salt = bcrypt.genSaltSync(10);
+    const hashedPassword = bcrypt.hashSync(password, salt);
+
+    const user = {
+      name,
+      email,
+      password: hashedPassword,
+    };
+
+    try {
+      await User.create(user);
+      req.flash("message", "Cadastro realizado com sucesso!");
+      res.redirect("/");
+    } catch (error) {
+      console.log(error);
+    }
   }
 };
